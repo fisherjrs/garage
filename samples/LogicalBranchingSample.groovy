2fa57e8c341c33f002e9c8c37a55d40fcd9bf672
fisherjrs <fisher.j.r.s@gmail.com>
2014-11-27 07:24:12 -0600

    Assertions, beans and logical samples

--- samples/LogicalBranchingSample.groovy
+++ samples/LogicalBranchingSample.groovy
@@ -16,3 +16,44 @@ if ( x ) {
 }
 
 assert x == y
+
+
+def s = 5
+def t = (s > 1) ? "worked" : "failed"
+assert t == "worked"
+println("did it work ${t}")
+
+
+def p = 1.23
+def result = ""
+
+switch ( p ) {
+	case "foo":
+		result = "found foo"
+		// lets fall through
+
+	case "bar":
+		result += "bar"
+
+	case [4, 5, 6, 'inList']:
+		result = "list"
+		break
+
+	case 12..30:
+		result = "range"
+		break
+
+	case Integer:
+		result = "integer"
+		break
+
+	case Number:
+		result = "number"
+		break
+
+	default:
+		result = "default"
+}
+
+assert result == "number"
+println("result :: ${result}")
