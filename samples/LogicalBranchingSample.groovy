fb51a620d4b4ceb5b2d7a731bcb675a7bc1ceb8e
fisherjrs <fisher.j.r.s@gmail.com>
2014-11-28 13:43:50 -0600

    more switch statements

--- samples/LogicalBranchingSample.groovy
+++ samples/LogicalBranchingSample.groovy
@@ -24,36 +24,55 @@ assert t == "worked"
 println("did it work ${t}")
 
 
-def p = 1.23
+def p = 'foo'
 def result = ""
 
 switch ( p ) {
 	case "foo":
 		result = "found foo"
+		println("result :: ${result}")
 		// lets fall through
-
+	case ["corn", "foo", "grass", 'inList']:
+		result += " and found foo in list"
 	case "bar":
 		result += "bar"
+		println("result :: ${result}")
 
 	case [4, 5, 6, 'inList']:
 		result = "list"
+		println("result :: ${result}")
 		break
 
 	case 12..30:
 		result = "range"
+		println("result :: ${result}")
 		break
 
 	case Integer:
 		result = "integer"
+		println("result :: ${result}")
 		break
 
 	case Number:
 		result = "number"
+		println("result :: ${result}")
 		break
 
 	default:
 		result = "default"
+		println("result :: ${result}")
 }
 
 assert result == "number"
 println("result :: ${result}")
+
+h = 3
+if ( [2,3,4,5, 'inList']) {
+	println('found in list')
+}
+
+d = "fred"
+
+if(['fred', 'tom', 'shelly', 'inList']) {
+	println('found in list of strings.')
+}
